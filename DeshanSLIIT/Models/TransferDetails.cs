0449a7df8d4849e8da5e460d05bf6debd404eb47
venom0039 <65506774+venom0039@users.noreply.github.com>
2022-02-05 19:53:19 +0530

    Commit 3

--- DeshanSLIIT/Models/TransferDetails.cs
+++ DeshanSLIIT/Models/TransferDetails.cs
@@ -3,7 +3,6 @@
     public class TransferDetails
     {
         public string Location { get; set; }
-        public string Cat { get; set; }
         public Category CatData { get; set; }
     }
 }
